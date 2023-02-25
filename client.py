57edadbe614b540c0dc82879fcac5d894e0add13
Danila Krasov <67839759+Fr0stFree@users.noreply.github.com>
2023-02-25 10:16:14 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,10 @@
+# import asyncio
+# import aiohttp
+
+
+class Client:
+    def __init__(self, server_host="127.0.0.1", server_port=8000):
+        pass
+
+    def send(self, message=""):
+        pass
---
+++ server.py
@@ -0,0 +1,9 @@
+# import asyncio
+
+
+class Server:
+    def __init__(self, host="127.0.0.1", port=8000):
+        pass
+
+    def listen(self):
+        pass
